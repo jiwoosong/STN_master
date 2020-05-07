6d15b654d93ba742b5b33dfe62c76e0ed36c5f1f
jiwoosong <woojisong@naver.com>
2020-05-07 20:33:22 +0900

    Changing .gitignore to ignore gpu*.py files. Merging get_gaussian_filter.py into Global_weight_AP.py Delete gpu*.py files.

--- Networks/classifier/Global_weight_AP.py
+++ Networks/classifier/Global_weight_AP.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import os
 from scipy import ndimage
-from Networks.classifier.get_gaussian_filter import get_gaussian_kernel
+# from Networks.classifier.get_gaussian_filter import get_gaussian_kernel
 import  math
 
 class Classification_net(nn.Module):
@@ -29,3 +29,31 @@ class Classification_net(nn.Module):
         x = torch.mul(x, g_filter)
         x = torch.mean(x.view(x.size(0), x.size(1), -1), dim=2)
         return x
+
+    def get_gaussian_kernel(self, zero_grid, kernel_size=50, sigma=2, channels=3):
+        mean = (kernel_size - 1) / 2.
+        variance = sigma ** 2.
+
+        # Calculate the 2-dimensional gaussian kernel which is
+        # the product of two gaussian distributions for two different
+        # variables (in this case called x and y)
+        gaussian_kernel = (1. / (2. * math.pi * variance)) * \
+                          torch.exp(
+                              -torch.sum((zero_grid - mean) ** 2., dim=-1) / \
+                              (2 * variance)
+                          )
+
+        # Make sure sum of values in gaussian kernel equals 1.
+        gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
+
+        # Reshape to 2d depthwise convolutional weight
+        gaussian_kernel = gaussian_kernel.view(1, 1, kernel_size, kernel_size)
+        gaussian_kernel = gaussian_kernel.repeat(channels, 1, 1, 1)
+
+        gaussian_filter = nn.Conv2d(in_channels=channels, out_channels=channels,
+                                    kernel_size=kernel_size, groups=channels, bias=False)
+
+        gaussian_filter.weight.data = gaussian_kernel
+        gaussian_filter.weight.requires_grad = False
+
+        return gaussian_filter
