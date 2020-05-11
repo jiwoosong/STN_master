6eaadaf66ef353b76cf1c6a46c120c7a557ddace
jiwoosong <woojisong@naver.com>
2020-05-11 13:00:40 +0900

    Change algin_corner option in Linearized.py

--- Networks/warper/Basic/Linearized.py
+++ Networks/warper/Basic/Linearized.py
@@ -4,14 +4,15 @@ from modules.Linearized_module import linearized_transform
 
 
 class Linearized(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, align_corners):
         super(Linearized, self).__init__()
         self.level_size = []
         self.name = os.path.realpath(__file__).split('/')[-1]
+        self.align_corners = align_corners
         # print(jutils.toCyan('-----------------------------------------'))
         # print(jutils.toCyan('Warping Module : Linearized'))
 
     def forward(self, image, pMtrx, W, H):
-        warped_image = linearized_transform(image, pMtrx, W=self.W, H=self.H)
+        warped_image = linearized_transform(image, pMtrx, W=self.W, H=self.H, align_corners=self.align_corners)
 
         return warped_image
