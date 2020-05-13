2637494136275b7d93513202cdbd70266d60fe1c
dlagkrtns <clear_learn@naver.com>
2020-05-13 18:09:37 +0900

    The number of convolutions has changed

--- Networks/classifier/GMP.py
+++ Networks/classifier/GMP.py
@@ -8,12 +8,16 @@ class Classification_net(nn.Module):
         super(Classification_net, self).__init__()
         self.name = os.path.realpath(__file__).split('/')[-1]
         self.conv1 = nn.Conv2d(in_chanel, 128, kernel_size=[1, 1], stride=1, padding=0)
-        self.conv2 = nn.Conv2d(128, out_chanel, kernel_size=[1, 1], stride=1, padding=0)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=[1, 1], stride=1, padding=0)
+        self.conv3 = nn.Conv2d(256, 512, kernel_size=[1, 1], stride=1, padding=0)
+        self.conv4 = nn.Conv2d(512, out_chanel, kernel_size=[1, 1], stride=1, padding=0)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.relu(self.conv1(x))
         x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.conv4(x))
         x = F.max_pool2d(x, kernel_size=x.size()[2:])
         x = x.view(64, 43)
         return x
